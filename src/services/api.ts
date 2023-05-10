e9a35a2bce332d8b0da192638f4260a4b55b5c70
Elias Neto <eliasneto321@outlook.com>
2023-05-10 19:38:11 -0300

    update api baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "https://json-server-24r6.onrender.com",
+  baseURL: "https://json-server-xi-eight.vercel.app/",
 })
 
 export default api
